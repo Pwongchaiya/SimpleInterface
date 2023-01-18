e9bd62458677e04817bc881d07736ef4b89483e5
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-01-17 22:14:17 -0800

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,5 @@
+public interface Character {
+    String username(String name);
+    String getCharacterHighScore();
+    void openCharacterHistory();
+}
---
+++ Player.java
@@ -0,0 +1,24 @@
+public class Player implements Character{
+
+    String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String username(String name) {
+        return name+" is being displayed.";
+    }
+
+    @Override
+    public String getCharacterHighScore() {
+        return "Highest kills: 10000";
+    }
+
+    @Override
+    public void openCharacterHistory() {
+        System.out.println(username(name)+
+                "\n"+getCharacterHighScore());
+    }
+}
